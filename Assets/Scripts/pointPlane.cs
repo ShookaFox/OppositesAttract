6ef7d586c9bc6f3cca49c981fda7d136cf95768c
Aidan Lovelace <aidan@aidanlovelace.com>
2018-01-08 21:38:27 -0600

    Organized the project and improved the game mechanics.
    * Mainly, I improved the physics and the textures and the goal detection.

---
+++ Assets/Scripts/pointPlane.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointPlane : MonoBehaviour {
+	public GoalType goal;
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "ball")
+			if (goal == GoalType.RED)
+				ScoreEngine.redScore++;
+			else
+				ScoreEngine.blueScore++;
+	}
+}
+public enum GoalType {
+	RED,
+	BLUE
+}
