059ad917209e0cc5e923578f62ca320a9990afc7
Seth Painter <timytomy11@gmail.com>
2018-01-10 16:52:23 -0600

    The Walls now repel the ball so it doesn't get stuck.
    By Aidan Lovelace on Seth's computer.

---
+++ Assets/Scripts/WallRepel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRepel : MonoBehaviour
+{
+	void OnCollisionEnter(Collision collision)
+	{
+		Vector3 newVelocity = collision.relativeVelocity.normalized;
+		newVelocity.x = -newVelocity.x * 2;
+		newVelocity.y = -newVelocity.y * 2;
+		newVelocity.z = -newVelocity.z * 2;
+		collision.collider.attachedRigidbody.velocity = newVelocity;
+	}
+}
