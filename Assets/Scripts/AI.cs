e928762d9f5bb13857fe2f6e4961e7dfa45ebc0d
Seth Painter <timytomy11@gmail.com>
2018-02-21 16:37:54 -0600

    Follow AI

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+	public Transform Character; // Target Object to follow
+	public float speed=0.1F; // Enemy speed
+	private Vector3 directionOfCharacter;
+
+	void Update() {
+		directionOfCharacter = Character.transform.position - transform.position;
+		directionOfCharacter = directionOfCharacter.normalized;    // Get Direction to Move Towards
+		transform.Translate (directionOfCharacter * speed, Space.World);
+	}
+}
